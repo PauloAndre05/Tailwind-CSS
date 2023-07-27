fd9c4a5a3582d2c19a7539e64d8fc035ab829681
PauloAndre05 <pauloandrepaulinho05@gmail.com>
2023-07-27 01:50:27 +0100

    Finish screen login of GitHub

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo  from './assets/img/logoGitHub.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="w-full h-screen flex flex-col items-center ">
+      <section className='flex flex-col items-center mt-12'>
+        <div className='w-[48px]' > <img src={logo} alt="Logo git" className='object-cover w-full'/> </div>
+        <h2 className='text-[24px] font-light m-6'>Sign in to GitHub</h2>
+        <div>
+          <form className='bg-[#F6F8FA] flex flex-col p-4 gap-5 border rounded-[.4rem]'>
+            <div className='flex flex-col w-[274px]'>
+              <label htmlFor="userName" className='text-[14px] pb-2'>Username or email address</label>
+              <input type="text" id='userName' className='rounded-[.4rem] h-8 border'/>
+            </div>
+
+            <div className='flex flex-col w-[274px]'>
+              <div className='w-full flex justify-between text-[14px] pb-2'><label htmlFor="password">Password</label> <a href="#" className='text-[12px] text-[#0969da]'>Forgot password?</a></div>
+              <input type="text" id='userName' className='rounded-[.4rem] h-8 border'/>
+            </div>
+
+            <div className='w-274px[]'>
+              <button className='border w-full h-8 bg-[#1f883d] text-white  rounded-[.4rem] text-[14px]'>Sign in</button>
+            </div>
+          </form>
+          <div className='flex border mt-4 rounded-[.5rem] justify-center items-center p-4 text-[14px]'>
+            <p>New to GitHub? <a href="#" className='text-[#0969da]'>Create an account</a>.</p> 
+          </div>
+        </div>
+      </section>
+
+      <section className='flex gap-4 text-[12px] text-[#0969da] mt-20'>
+        <a href="#">Terms</a>
+        <a href="#">Privacy</a>
+        <a href="#">Security</a>
+        <a href="#" className='text-gray-600'>Contact GitHub</a>
+      </section>
+
+      <footer className='mt-8 text-[#1f883d] text-[1.5rem] font-bold'>P.G</footer>  
+    </main>
+    
   )
 }
 
